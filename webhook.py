ebe2e1ef9f1cf9a80f20467e6d06d8adb1b2eb22
jsonia <91981845+jsonia@users.noreply.github.com>
2022-11-17 13:38:37 -0800

    logs and web-hook for downlink added

--- webhook.py
+++ webhook.py
@@ -20,19 +20,6 @@ def webhook3():
     app.logger.info('Info level log')
     if(request.method == 'POST'):
         print(request.json)
-        url = 'https://lorala.nam1.cloud.thethings.industries/api/v3/as/applications/my-application/webhooks/test-webhook/devices/my-device/down/push'
-        headers = {'Authorization': "Bearer NNSXS.QYNVIPCESUULZ73YK2CGM2KRBGSTIGVJXCZMRSY.CVA4L4P47W7PPYUTACCDOTR5DGKN3QGJMN2HFJ6AYNOPILY7KI4A",
-       'Content-Type': "application/json",
-       'User-Agent': "my-integration/my-integration-version"}
-        data = {"downlinks":[{
-      "frm_payload":"vu8=",
-      "f_port":2,
-      "priority":"NORMAL"
-    }]
-  }
-        res = requests.post(url, json=data, headers=headers)
-        print (res.status_code)
-        print (res.raise_for_status())
     return 'success', 200
 
 @app.route('/uplinks', methods=['POST'])
@@ -54,8 +41,8 @@ def webhook1():
         print (res.raise_for_status())
         return 'success', 200
 
-
-@app.route('/downlinks', methods=['POST'])
+#for downlink    
+@app.route('/', methods=['POST'])
 def webhook2():
     if(request.method == 'POST'):
         print(request.json)
