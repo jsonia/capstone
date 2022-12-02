407b5e9f848934904cec437d6c9a702f7395b16f
jsonia <91981845+jsonia@users.noreply.github.com>
2022-12-01 23:35:32 -0800

    Update device.py

--- device.py
+++ device.py
@@ -21,10 +21,17 @@ lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 
 while not lora.has_joined():
     time.sleep(2.5)
-    print('Not yet joined...')     
+    print('Not yet joined...')
+
+
+# def lora_rx(lora):  
+#     events = lora.events()
+#     if events & LoRa.RX_PACKET_EVENT:
+#         t = time.gmtime()
+#         print('Lora packet received', t)        
 
 print('Joined')
-
+# create a LoRa socket
 def lora_cb(lora):  
     events = lora.events()
     if events & LoRa.TX_FAILED_EVENT:
@@ -35,15 +42,16 @@ def lora_cb(lora):
         print('Lora packet sent', t)
     if events & LoRa.RX_PACKET_EVENT:
         t = time.gmtime()
-        print('Lora packet received', t)     
+        print('Lora packet received', t)
+          
 
 lora.callback(trigger=(LoRa.RX_PACKET_EVENT | LoRa.TX_PACKET_EVENT | LoRa.TX_FAILED_EVENT), handler=lora_cb)
     
-# create a LoRa socket
+
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
 s.setsockopt(socket.SOL_LORA, socket.SO_CONFIRMED, 1)
-s.setblocking(False)
+s.setblocking(True)
 
 s.send(bytes([0x01, 0x02, 0x03]))
 
@@ -60,3 +68,6 @@ while(1):
     #print('downlink received', t.tm_hour, ' ', t.tm_min, ' ', t.tm_sec)
     
     time.sleep(1);
+
+
+
